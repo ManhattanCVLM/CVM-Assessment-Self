8c087c672032dc9880a5d6df9b1b3bc056b0812e
ManhattanCVLM <peteafischer@googlemail.com>
2026-08-18 10:46:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
    change, and they change with the cache name when they do.
 
    Bump CACHE whenever index.html changes. */
-const CACHE = "cvm-self-v19";
+const CACHE = "cvm-self-v20";
 const ASSETS = [
   "./",
   "./index.html",
